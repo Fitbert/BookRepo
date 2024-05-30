f9ad79d5f24096235b2b278864bbaa4173e37699
Albert T <albert.terrazas@gmail.com>
2024-05-30 15:16:15 -0500

    mutations js file

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type BookInput {
+    authors: [String]
+    description: String
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, password: String!, email: String!): Auth
+    saveBook(bookId: ID!): Book
+    removeBook(bookId: ID!): Book
+  }
+  `;
+  
+module.exports = typeDefs;
