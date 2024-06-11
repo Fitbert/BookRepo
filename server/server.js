9b01a998364fbc3867a1959931f48fd685a56eab
Albert T <albert.terrazas@gmail.com>
2024-06-10 21:04:56 -0500

    change serve.js

--- server/server.js
+++ server/server.js
@@ -3,16 +3,15 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-const require('dotenv')
+require('dotenv').config(); // Corrected dotenv require statement
 const { typeDefs, resolvers } = require('../server/schema');
 const db = require('./config/connection');
-const cors = require('cors')
+const cors = require('cors');
+
 const corsOptions = {
-    origin: '*',
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    accessControlAllowOrigin: '*',
-    accessControlAllowCredentials: true,
-}
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,29 +24,28 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
-  app.use(cors(corsOptions))
+  app.use(cors(corsOptions));
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware,
+  }));
 
-app.use('/graphql', expressMiddleware(server, {
-  context: authMiddleware
-}));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`); // Corrected URL to http
+    });
   });
-}
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at https://localhost:${PORT}/graphql`);
-  });
-});
 };
 
 // Call the async function to start the server
